44c9e35c2500078fec52e0219ab60108555269b7
monakouzi <mo.nakouzi03@gmail.com>
2023-12-13 20:52:06 -0500

    Co-authored-by: William Khine <williamkhine@outlook.com>

---
+++ src/app/_components/general/Navbar.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="bg-green-700 h-16 w-full">
+                <div>
+
+                </div>
+            </nav>
+        </>
+    )
+}
