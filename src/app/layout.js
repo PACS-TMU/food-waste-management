fb9933ec85b148da3ddc4459a2360a35d6a6ed3c
monakouzi <mo.nakouzi03@gmail.com>
2023-12-18 02:06:14 -0500

    Test OneSignal2

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,25 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
-          defer
-        ></script>
-        <script>
-          window.OneSignalDeferred = window.OneSignalDeferred || [];
-          OneSignalDeferred.push(function(OneSignal){" "}
-          {OneSignal.init({
-            appId: "b26b5cc4-1879-4a63-b584-929ec6109184",
-            safari_web_id:
-              "web.onesignal.auto.1a6e6435-147d-4598-9ef8-2c64afd14980",
-            notifyButton: {
-              enable: true,
-            },
-          })}
-          );
-        </script>
-      </head>
       <body className="main">
         <Navbar />
         {children}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,23 @@
+'use client'
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+        OneSignal.init({
+            appId: "b26b5cc4-1879-4a63-b584-929ec6109184",
+            safari_web_id: "web.onesignal.auto.1a6e6435-147d-4598-9ef8-2c64afd14980",
+            notifyButton: {
+                enable: true,
+            },
+            allowLocalhostAsSecureOrigin: true,
+        });
+    });
+    return () => {
+        window.OneSignal = undefined;
+    };
+}, []); // <-- run this effect once on mount
   return (
       <div>
       </div>
