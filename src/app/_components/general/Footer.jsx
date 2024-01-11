c0b68cfee49c12f46abb97ebfca133e1e338eee7
monakouzi <mo.nakouzi03@gmail.com>
2024-01-10 22:23:55 -0500

    fixing footer nav buttons too small problem

--- src/app/_components/general/Footer.jsx
+++ src/app/_components/general/Footer.jsx
@@ -5,23 +5,23 @@ export default function Footer() {
     return (
         <footer className="fixed max-w-[768px] w-screen bottom-0 h-12">
             <div className="h-full bg-dark-green text-white grid grid-rows-1 grid-cols-4 py-3 px-2 justify-center items-center">
-                <button className="home flex h-full justify-center items-center border-r">
-                    <Link href="/">
+                <button className="home flex h-12 justify-center items-center border-r">
+                    <Link href="/" className="h-full w-full flex items-center justify-center">
                         <MdHome size={28} />
                     </Link>
                 </button>
-                <button className="group flex h-full justify-center items-center border-r">
-                    <Link href="/group">
+                <button className="group flex h-12 justify-center items-center border-r">
+                    <Link href="/group" className="h-full w-full flex items-center justify-center">
                         <MdGroups size={28} />
                     </Link>
                 </button>
-                <button className="settings flex h-full justify-center items-center border-r">
-                    <Link href="/settings">
+                <button className="settings flex h-12 justify-center items-center border-r">
+                    <Link href="/settings" className="h-full w-full flex items-center justify-center">
                         <MdSettings size={28} />
                     </Link>
                 </button>               
-                <button className="account flex h-full justify-center items-center">
-                    <Link href="/account">
+                <button className="account flex h-12 justify-center items-center">
+                    <Link href="/account" className="h-full w-full flex items-center justify-center">
                         <MdAccountCircle size={28} />
                     </Link>
                 </button>
