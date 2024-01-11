85caafacf403963414979f8b9a5d5a0e7f4c709d
monakouzi <mo.nakouzi03@gmail.com>
2024-01-10 23:34:14 -0500

    minor changes

--- src/app/account/scavenger-hunt/code/thank-you/page.jsx
+++ src/app/account/scavenger-hunt/code/thank-you/page.jsx
@@ -5,10 +5,10 @@ export default function ThankYou() {
     return (
         <div className="flex items-center justify-center p-4 h-[70vh] flex-col w-[95%] mx-auto py-4 mt-20 mb-20">
             <p className="text-3xl font-semibold text-center p-4">That&apos;s all Folks!</p>
-            <p className="text-lg text-left px-4 py-2">
+            <p className="text-lg text-left px-2 py-2">
                 Please speak to one of the VIRO X PACS team members to get a small thank you prize!
             </p>
-            <p className="text-lg text-left py-2 px-4">
+            <p className="text-lg text-left py-2 px-2">
                 Thank you for participating in our scavenger hunt and app demo, and we hope to see you at our next events! Good luck in the upcoming winter semester!
             </p>
             <div className="flex items-center justify-center flex-row my-6">
@@ -17,6 +17,7 @@ export default function ThankYou() {
                     width={150}
                     height={150}
                     alt="PACS Logo"
+                    priority={true}
                 />
                 <IoCloseSharp size={24} className="mx-2" />
                 <Image
@@ -24,6 +25,7 @@ export default function ThankYou() {
                     width={150}
                     height={150}
                     alt="VIRO Logo"
+                    priority={true}
                 />
             </div>
         </div>
